a923e89c394022563a97d49dec7e91d301bf5066
xav02135 <xav.dx02@gmail.com>
2025-07-04 14:36:55 +0200

    php test

---
+++ dbbphp/index.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'db';
+$dbname = 'rpg';
+$user = 'root';
+$password = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    // Active les erreurs PDO en exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie !";
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+
+
+
+?>
